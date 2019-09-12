2fe7641a75998f730c31cd32abed700f56f78798
Tony Edgin <tedgin@iplantcollaborative.org>
2019-09-12 13:35:32 -0700

    iquest -z option

--- completions/iquest.fish
+++ completions/iquest.fish
@@ -1,11 +1,40 @@
 # tab completion for iquest
 
+#
+# Condition Functions
+#
+
+function __iquest_no_opts
+  set args (__irods_tokenize_cmdline hz '')
+  for opt in $argv
+    if contains -- $opt $args
+      return 1
+    end
+  end
+  return 0
+end
+
+
+#
+# Completions
+#
+
 complete --command iquest --short-option h \
   --description 'shows help' \
   --condition '__irods_no_args_condition' --exclusive
 
+complete --command iquest --short-option z \
+  --description 'the zone to query' \
+  --condition '__iquest_no_opts -h -z' --exclusive
+
 # TODO implement
-# iquest [-z Zonename] [--no-page] [hint] [format] selectConditionString
+# iquest [-z Zonename]
+
+# TODO implement
+# iquest [--no-page]
+# iquest selectionConditionString
+# iquest [hint]
+# iquest [format]
 
 # TODO implement
 # iquest --sql 'pre-defined SQL string' [format] [arguments]
