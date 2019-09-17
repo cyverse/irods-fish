ad7d5b5068af4415dc86bbc4a4b04ecb000feee4
Tony Edgin <tedgin@iplantcollaborative.org>
2019-09-17 09:18:58 -0700

    irm -h completion

--- completions/irm.fish
+++ completions/irm.fish
@@ -1,7 +1,21 @@
 # tab completion for irm
 #
-# TODO extend to cover options
 # TODO make suggest appropriate arguments
 # TODO make suggest multiple arguments, if applicable
 
-complete --command irm --no-files --arguments '(__irods_path_suggestions)'
+# irm -h
+complete --command irm --short-option h \
+  --condition '__irods_no_args_condition (__irods_tokenize_cmdline hfrUvV n)' \
+  --description 'shows help'
+
+# irm [-f] [-r] [-U] [(-v|-V)] [--empty] [-n <repl-num>] (<collection>|<data-object>)
+
+# irm (<collection>|<data-object>)
+complete --command irm --arguments '(__irods_path_suggestions)' --no-files
+
+# TODO irm -f (<collection>|<data-object>)
+# TODO irm -r <collection>
+# TODO irm -U (<collection>|<data-object>)
+# TODO irm (-v|-V) (<collection>|<data-object>)
+# TODO irm --empty (<collection>|<data-object>)
+# TODO irm -n <repl-num> (<collection>|<data-object>)
