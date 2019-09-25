2ab35c40f8d93e14b6328e4c0e66c7e017a91c8c
Tony Edgin <tedgin@iplantcollaborative.org>
2019-09-25 11:18:44 -0700

    icd -h completion

--- completions/icd.fish
+++ completions/icd.fish
@@ -1,7 +1,13 @@
 # tab completion for icd
 #
-# TODO extend to cover options
-# TODO make suggest appropriate arguments
-# TODO make suggest multiple arguments, if applicable
+# TODO verify if order matters
 
+# icd -h
+complete --command icd --short-option h \
+  --condition "__irods_no_args_condition (__irods_tokenize_cmdline h '')" \
+  --description 'shows help'
+
+# TODO icd [(-V|-v)] <collection>
+# TODO restrict to collections
+# TODO restrict to single path
 complete --command icd --no-files --arguments '(__irods_path_suggestions)'
