fef5b3010de6ac8b16e074cfd6a0b98bc4cd360f
Tony Edgin <tedgin@iplantcollaborative.org>
2019-09-25 13:05:18 -0700

    extracted out common icd cmdline parsing logic

--- completions/icd.fish
+++ completions/icd.fish
@@ -1,8 +1,21 @@
 # tab completion for icd
 
+#
+# Helper Functions
+#
+
+function __icd_tokenize_cmdline
+  __irods_tokenize_cmdline hV ''
+end
+
+
+#
+# Completions
+#
+
 # icd -h
 complete --command icd --short-option h \
-  --condition "__irods_no_args_condition (__irods_tokenize_cmdline hV '')" \
+  --condition '__irods_no_args_condition (__icd_tokenize_cmdline)' \
   --description 'shows help'
 
 # TODO restrict to collections
@@ -12,7 +25,7 @@ complete --command icd --arguments '(__irods_path_suggestions)' --no-files
 
 # icd -V <collection>
 complete --command icd --short-option V \
-  --condition "__irods_tokenize_cmdline hV '' | __irods_missing -h -V" \
+  --condition '__icd_tokenize_cmdline | __irods_missing -h -V' \
   --description 'very verbose'
 
 # TODO icd -v <collection>
