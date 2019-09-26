6d0200de482100306a10a13f2b31fca7edf084f1
Tony Edgin <tedgin@iplantcollaborative.org>
2019-09-26 15:39:46 -0700

    imeta -V completion

--- completions/imeta.fish
+++ completions/imeta.fish
@@ -7,8 +7,17 @@
 
 # imeta -h
 complete --command imeta --short-option h \
-  --condition "__irods_no_args_condition (__irods_tokenize_cmdline h '')" \
+  --condition "__irods_no_args_condition (__irods_tokenize_cmdline hV '')" \
   --description 'shows help'
 
-# TODO imeta [(-V|-v)][-z <zone>][command]
+# TODO imeta -V
+complete --command imeta --short-option V \
+  --condition "__irods_tokenize_cmdline hV '' | __irods_missing -h -V" \
+  --description 'very verbose'
+
+# TODO imeta -v
+
+# TODO imeta -z <zone>
+
+# TODO imeta <command>
 complete --command imeta --arguments '(__irods_exec_slow __irods_path_suggestions)' --no-files
