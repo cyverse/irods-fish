b1c6e72269e555cc71ad7a495b45468ca6e484c4
Tony Edgin <tedgin@iplantcollaborative.org>
2019-09-26 15:35:49 -0700

    imeta -h completion

--- completions/imeta.fish
+++ completions/imeta.fish
@@ -1,7 +1,14 @@
 # tab completion for imeta
 #
+# TODO determine allowed order or arguments
 # TODO extend to cover options
 # TODO make suggest appropriate arguments
 # TODO make suggest multiple arguments, if applicable
 
-complete --command imeta --arguments '(__irods_exec_slow __irods_path_suggestions)' --no-files 
+# imeta -h
+complete --command imeta --short-option h \
+  --condition "__irods_no_args_condition (__irods_tokenize_cmdline h '')" \
+  --description 'shows help'
+
+# TODO imeta [(-V|-v)][-z <zone>][command]
+complete --command imeta --arguments '(__irods_exec_slow __irods_path_suggestions)' --no-files
