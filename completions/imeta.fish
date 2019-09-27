1ba65c54add52fb71cd64a91709590ae6b8da8ff
Tony Edgin <tedgin@iplantcollaborative.org>
2019-09-27 13:16:59 -0700

    imeta -z completion

--- completions/imeta.fish
+++ completions/imeta.fish
@@ -9,8 +9,39 @@
 # Helper Functions
 #
 
+function __imeta_needs_zone
+  __irods_cmdline_needs_param_val -z $argv
+end
+
 function __imeta_tokenize_cmdline
-  __irods_tokenize_cmdline hVv ''
+  __irods_tokenize_cmdline hVv z
+end
+
+
+#
+# Condition functions
+#
+
+function __imeta_suggest_zone
+  set args (__imeta_tokenize_cmdline)
+  if echo $args | __irods_missing -h
+    if set zIdx (contains --index -- -z $args)
+      __imeta_needs_zone $args; and test "$args[-1]" != -
+    else
+      true
+    end
+  else
+    false
+  end
+end
+
+
+#
+# Suggestion functions
+#
+
+function __imeta_zone_suggestions
+  iquest --no-page '%s' 'select ZONE_NAME' | string match --invert 'CAT_NO_ROWS_FOUND:*'
 end
 
 
@@ -21,7 +52,11 @@ end
 __irods_help_completion imeta
 __irods_verbose_completion imeta '__imeta_tokenize_cmdline | __irods_missing -h -V -v'
 
-# TODO imeta -z <zone>
+# imeta -z <zone>
+complete --command imeta --short-option z \
+--arguments '(__irods_exec_slow __imeta_zone_suggestions)' --exclusive \
+--condition __imeta_suggest_zone \
+--description 'work with the specified zone'
 
 # TODO imeta <command>
 complete --command imeta --arguments '(__irods_exec_slow __irods_path_suggestions)' --no-files
