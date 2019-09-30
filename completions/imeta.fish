60ea286cee5816bba8aa1ad550845f22bc49dee2
Tony Edgin <tedgin@iplantcollaborative.org>
2019-09-30 14:27:44 -0700

    imeta command name completion

--- completions/imeta.fish
+++ completions/imeta.fish
@@ -22,9 +22,23 @@ end
 # Condition functions
 #
 
+function __imeta_suggest_cmd
+  set opts \
+    (fish_opt --short h) (fish_opt --short V) (fish_opt --short v) (fish_opt --short z --required)
+  set argv (commandline --cut-at-cursor --tokenize)
+  set --erase argv[1]
+  if not argparse --stop-nonopt --name=imeta $opts -- $argv 2> /dev/null
+    false
+  else if set --query _flag_h
+    false
+  else
+    test (count $argv) -eq 0
+  end
+end
+
 function __imeta_suggest_zone
   set args (__imeta_tokenize_cmdline)
-  if echo $args | __irods_missing -h
+  if echo $args | __irods_missing -h add adda
     if set zIdx (contains --index -- -z $args)
       __imeta_needs_zone $args; and test "$args[-1]" != -
     else
@@ -49,14 +63,88 @@ end
 # Completions
 #
 
+# TODO consider converting to using something like __imeta_suggest_cmd
 __irods_help_completion imeta
-__irods_verbose_completion imeta '__imeta_tokenize_cmdline | __irods_missing -h -V -v'
 
+# TODO consider converting to using something like __imeta_suggest_cmd
+__irods_verbose_completion imeta '__imeta_tokenize_cmdline | __irods_missing -h -V -v add adda'
+
+# TODO consider converting to using something like __imeta_suggest_cmd
 # imeta -z <zone>
 complete --command imeta --short-option z \
---arguments '(__irods_exec_slow __imeta_zone_suggestions)' --exclusive \
---condition __imeta_suggest_zone \
---description 'work with the specified zone'
+  --arguments '(__irods_exec_slow __imeta_zone_suggestions)' --exclusive \
+  --condition __imeta_suggest_zone \
+  --description 'work with the specified zone'
 
-# TODO imeta <command>
+# imeta add
+complete --command imeta --arguments add --no-files --condition __imeta_suggest_cmd \
+  --description 'add new AVU triple'
+
+# TODO imeta add (-d|-C|-R|-u) <entity> <attribute> <value> [<unit>]
 complete --command imeta --arguments '(__irods_exec_slow __irods_path_suggestions)' --no-files
+
+# imeta adda
+complete --command imeta --arguments adda --condition __imeta_suggest_cmd \
+  --description 'administratively add new AVU triple'
+
+# TODO imeta adda (-d|-C|-R|-u) <entity> <attribute> <value> [<units>]
+
+# imeta addw
+complete --command imeta --arguments addw --condition __imeta_suggest_cmd \
+  --description 'add new AVU triple using wildcards in name'
+
+# TODO imeta addw (-d|-C|-R|-u) <entity> <attribute> <value> [<units>]
+
+# imeta cp
+complete --command imeta --arguments cp --condition __imeta_suggest_cmd \
+  --description 'copy AVUs from one item to another'
+
+# TODO imeta cp (-d|-C|-R|-u) (-d|-C|-R|-u) <from-entity> <to-entity>
+
+# imeta ls
+complete --command imeta --arguments ls --condition __imeta_suggest_cmd \
+  --description 'list existing AVUs'
+
+# TODO imeta ls (-d|-C|-R|-u) <entity> [<attribute>]
+
+# imeta lsw
+complete --command imeta --arguments lsw --condition __imeta_suggest_cmd \
+  --description 'list existing AVUs using wildcards'
+
+# TODO imeta lsw (-d|-C|-R|-u) <entity> [<attribute>]
+
+# imeta mod
+complete --command imeta --arguments mod --condition __imeta_suggest_cmd \
+  --description 'modify AVU'
+
+# TODO imeta mod (-d|-C|-R|-u) <entity> <attribute> <value> [<unit>][n:<new-attribute>][v:<new-value>][u:<new-units>]
+
+# imeta qu
+complete --command imeta --arguments qu --condition __imeta_suggest_cmd \
+  --description 'query entities with matching AVUs'
+
+# TODO imeta qu (-d|-C|-R|-u) <attribute> <op> <value> ...
+
+# imeta set
+complete --command imeta --arguments set --condition __imeta_suggest_cmd \
+  --description 'assign a single value'
+
+# TODO imeta set (-d|-C|-R|-u) <entity> <attribute> <new-value> [<new-units>]
+
+# imeta rm
+complete --command imeta --arguments rm --condition __imeta_suggest_cmd \
+  --description 'remove AVU'
+
+# imeta rmi
+complete --command imeta --arguments rmi --condition __imeta_suggest_cmd \
+  --description 'remove AVU by metadata id'
+
+# TODO imeta rmi (-d|-C|-R|-u) <entity> <metadata-id>
+
+# TODO imeta rm (-d|-C|-R|-u) <entity> <attribute> <value> [<units>]
+
+# imeta rmw
+complete --command imeta --arguments rmw --condition __imeta_suggest_cmd \
+  --description 'remove AVU using wildcards'
+
+# TODO imeta rmw (-d|-C|-R|-u) <entity> <attribute> <value> [<units>]
