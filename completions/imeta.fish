8d1afaed2695c8dd22f861c2b070b131622afc16
Tony Edgin <tedgin@iplantcollaborative.org>
2019-11-12 09:32:10 -0700

    imeta cp -R (-C|-d|-R|-u) <resc> completion

--- completions/imeta.fish
+++ completions/imeta.fish
@@ -479,6 +479,20 @@ function __imeta_cp_resc_dest_flag_cond --argument-names cmdline
   __imeta_parse_cmd_for condition cp $cmdline
 end
 
+function __imeta_cp_src_resc_cond --argument-names cmdline
+  function condition --no-scope-shadowing
+    if test "$_flag_R[1]" = 1 -a (count $_unparsed_args) -eq 0
+      test (count $_flag_R) -eq 2
+      or set --query _flag_C
+      or set --query _flag_d
+      or set --query _flag_u
+    else
+      false
+    end
+  end
+  __imeta_parse_cmd_for condition cp $cmdline
+end
+
 function __imeta_cp_to_coll_cond --argument-names cmdline
   function condition --no-scope-shadowing
     test "$_flag_C[-1]" = 2 -a (count $_unparsed_args) -eq 1
@@ -940,7 +954,10 @@ complete --command imeta \
 
 # cp -R
 __imeta_mk_flag_completions R 'from resource' '__irods_exec_slow __imeta_cp_admin_src_flag_cond'
-# TODO imeta cp -R (-C|-d|-R|-u) <from-resource>>
+complete --command imeta \
+  --arguments '(__imeta_eval_with_cmdline __irods_exec_slow __imeta_resc_args)' \
+  --condition '__imeta_eval_with_cmdline __imeta_cp_src_resc_cond' \
+  --description 'source resource'
 
 # cp -R -C
 __imeta_mk_flag_completions C 'to collection' __imeta_cp_resc_dest_flag_cond
