52ccf200a96308efc9423306281e28265a47eabf
Tony Edgin <tedgin@iplantcollaborative.org>
2019-11-18 15:02:49 -0700

    irods path suggestions uses irods split path now

--- functions/__irods_path_suggestions.fish
+++ functions/__irods_path_suggestions.fish
@@ -3,19 +3,19 @@
 # argument is the beginning of a relative path or there isn't one, the suggested
 # paths will all be relative the user's current working collection.
 
+
 function __irods_path_suggestions --argument-names sugBegin \
     --description 'suggests collections and data objects in iRODS'
 
-  # TODO replace with __irods_absolute_path
-  set dirName ''
-  if string match --quiet --regex -- / $sugBegin
-    set pathParts (string split --right --max 1 / $sugBegin)
-    set dirName $pathParts[1]/
+  set pathParts (__irods_split_path $sugBegin)
+  set dirName $pathParts[1]
+
+  if string match --quiet --regex -- '[^/]$' $dirName
+    set dirName $dirName/
   end
 
-  set entries (command ils $dirName)
-
-  string replace --filter --regex '^  (C- )?' '' $entries \
+  command ils $dirName \
+    | string replace --filter --regex '^  (C- )?' '' \
     | string replace --regex '^.*/(.*)' '${1}/' \
     | string replace --regex '^' $dirName
 end
