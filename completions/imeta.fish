b986bc39199f58c0c07c238a5b92195e49ffda57
Tony Edgin <tedgin@iplantcollaborative.org>
2020-01-10 13:36:03 -0700

    imeta mod -R <resc> <attr> <val> <unit> completion

--- completions/imeta.fish
+++ completions/imeta.fish
@@ -797,6 +797,10 @@ function __imeta_mod_resc_attr_val_cond --argument-names cmdline
   __imeta_parse_cmd_for '__imeta_cmd_has_flag_with_num_args _flag_R 2' mod $cmdline
 end
 
+function __imeta_mod_resc_avu_cond --argument-names cmdline
+  __imeta_parse_cmd_for '__imeta_cmd_has_flag_with_num_args _flag_R 3' mod $cmdline
+end
+
 
 #
 # Suggestion functions
@@ -1039,7 +1043,7 @@ function __imeta_given_resc_attr_val_args --argument-names cmdline
   __imeta_parse_any_cmd_for suggestions $cmdline
 end
 
-function __imeta_resc_attr_val_unit_args --argument-names cmdline
+function __imeta_any_resc_attr_val_unit_args --argument-names cmdline
   function suggestions --no-scope-shadowing
     set attr $_unparsed_args[2]
     set val $_unparsed_args[3]
@@ -1047,6 +1051,22 @@ function __imeta_resc_attr_val_unit_args --argument-names cmdline
     __irods_quest '%s' \
       "select META_RESC_ATTR_UNITS
        where META_RESC_ATTR_NAME = '$attr'
+         and META_RESC_ATTR_VALUE = '$val'
+         and META_RESC_ATTR_UNITS like '$unitPat'"
+  end
+  __imeta_parse_any_cmd_for suggestions $cmdline
+end
+
+function __imeta_given_resc_attr_val_unit_args --argument-names cmdline
+  function suggestions --no-scope-shadowing
+    set resc $_unparsed_args[1]
+    set attr $_unparsed_args[2]
+    set val $_unparsed_args[3]
+    set unitPat $_curr_token%
+    __irods_quest '%s' \
+      "select META_RESC_ATTR_UNITS
+       where RESC_NAME = '$resc'
+         and META_RESC_ATTR_NAME = '$attr'
          and META_RESC_ATTR_VALUE = '$val'
          and META_RESC_ATTR_UNITS like '$unitPat'"
   end
@@ -1248,7 +1268,7 @@ complete --command imeta \
   --condition '__imeta_eval_with_cmdline __imeta_adda_resc_attr_val_cond' \
   --description 'existing for attribute'
 complete --command imeta \
-  --arguments '(__imeta_eval_with_cmdline __irods_exec_slow __imeta_resc_attr_val_unit_args)' \
+  --arguments '(__imeta_eval_with_cmdline __irods_exec_slow __imeta_any_resc_attr_val_unit_args)' \
   --condition '__imeta_eval_with_cmdline __imeta_adda_resc_avu_cond' \
   --description 'existing for attribute-value'
 
@@ -1493,7 +1513,11 @@ complete --command imeta \
   --arguments '(__imeta_eval_with_cmdline __irods_exec_slow __imeta_given_resc_attr_val_args)' \
   --condition '__imeta_eval_with_cmdline __imeta_mod_resc_attr_val_cond' \
   --description 'current value'
-# TODO imeta mod -R <resc> <attr> <val> <unit>
+complete --command imeta \
+  --arguments \
+    '(__imeta_eval_with_cmdline __irods_exec_slow __imeta_given_resc_attr_val_unit_args)' \
+  --condition '__imeta_eval_with_cmdline __imeta_mod_resc_avu_cond' \
+  --description 'current unit'
 
 # TODO imeta mod -R <resc> <attr> <val> \
 #        ( n:<new-attr> [(v:<new-val> [u:<new-units>]|u:<new-units> [v:<new-val>])]  | \
