32cb9e4769946429c92ccc02394c96be6bdec462
Tony Edgin <tony.edgin@gmail.com>
2020-03-20 11:02:57 -0700

    switched to tab indentation for env completions

--- environment/completions/ido.fish
+++ environment/completions/ido.fish
@@ -1,14 +1,16 @@
 # tab completion for ido
 
 function __ido_suggestions
-  set cmdTerms (commandline --cut-at-cursor --tokenize)
-  if test (count $cmdTerms ) -eq 1
-    ienvs
-  else
-    set environ $cmdTerms[2]
-    set cmd (string replace --regex -- '\s*ido\s+'"$environ"'\s+' '' (commandline))
-    ido $environ complete "--do-complete='$cmd'"
-  end
+	set cmdTerms (commandline --cut-at-cursor --tokenize)
+
+	if [ (count $cmdTerms) -eq 1 ]
+		ienvs
+	else
+		set environ $cmdTerms[2]
+		set cmd (string replace --regex -- '\s*ido\s+'"$environ"'\s+' '' (commandline))
+		ido $environ complete "--do-complete='$cmd'"
+	end
 end
+
 
 complete --command ido --no-files --arguments '(__ido_suggestions)'
--- environment/completions/iswitch.fish
+++ environment/completions/iswitch.fish
@@ -1,9 +1,10 @@
 # autocompletions for iswitch
 
 function __iswitch_suggestions
-  if test (count (commandline --cut-at-cursor --tokenize)) -eq 1
-    ienvs
-  end
+	if [ (count (commandline --cut-at-cursor --tokenize)) -eq 1 ]
+		ienvs
+	end
 end
+
 
 complete --command iswitch --no-files --arguments '(__iswitch_suggestions)'
