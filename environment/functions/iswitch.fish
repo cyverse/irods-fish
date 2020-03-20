2a1ae367d3d4c4565a3e2de41a164c57441fba40
Tony Edgin <tony.edgin@gmail.com>
2020-03-20 10:59:00 -0700

    added help to iswitch

--- environment/functions/iswitch.fish
+++ environment/functions/iswitch.fish
@@ -1,20 +1,37 @@
-function iswitch --argument-names environ \
+function iswitch \
 		--description 'switches to the given iRODS environment'
 
-	if test (count $argv) -eq 0
-		printf 'Usage: iswitch ENVIRON\n'
-		printf 'Switch to using the iRODS environment ENVIRON. If ENVIRON is empty, i.e., '', the\n'
-		printf 'current environment is unset\n\n'
-		printf 'Call `ienvs` to list the available iRODS environments.\n'
-		return 1
-	end >&2
+	function show_help
+		echo 'Usage: iswitch [(-h|--help|ENVIRON)]
+This command switches the iRODS environment to ENVIRON. If ENVIRON is empty or
+not provided, the environment is unset.
+Options are:
+ -h | --help  displays this help'
+	end
 
-	if test -z "$environ"
-		set --erase IRODS_ENVIRONMENT_FILE
+	argparse --name iswitch (fish_opt --short h --long help) -- $argv
+	or begin
+		show_help >&2
+		return 1
+	end
+
+	if set --query _flag_h
+		show_help
+		return 0
+	end
+
+	set environ ''
+	if [ (count $argv) -gt 0 ]
+		set environ $argv[1]
+	end
+
+	if [ -z $environ ]
+		not set --query IRODS_ENVIRONMENT_FILE
+		or set --erase IRODS_ENVIRONMENT_FILE
 	else
 		set ieFile $HOME/.irods/$environ.json
 
-		if test -e "$ieFile"
+		if [ -e "$ieFile" ]
 			set --export --global IRODS_ENVIRONMENT_FILE $ieFile
 		else
 			printf 'The environment %s doesn\'t exist, or its file %s isn\'t readable.\n' \
