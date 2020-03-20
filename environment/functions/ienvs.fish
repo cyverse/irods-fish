43d4a5a3b4d3fda87114151af5d5a6a88367c224
Tony Edgin <tony.edgin@gmail.com>
2020-03-20 10:19:25 -0700

    ienvs shows help when bad command

--- environment/functions/ienvs.fish
+++ environment/functions/ienvs.fish
@@ -2,16 +2,19 @@ function ienvs \
 	--description 'lists the available iRODS environments'
 
 	function show_help
-		echo 'Usage: ienvs [-h|--help]
+		echo "Usage: ienvs [-h|--help]
 This command lists the names of the available iRODS environments. An environment
-is defined by the contents of an iRODS environment file with the environment\'s
+is defined by the contents of an iRODS environment file with the environment's
 name being the base name of the file without the .json file extension.
 Options are:
- -h | --help  displays this help'
+ -h | --help  displays this help"
 	end
 
 	argparse --name ienvs (fish_opt --short h --long help) -- $argv
-	or return 1
+	or begin
+		show_help >&2
+		return 1
+	end
 
 	if set --query _flag_h
 		show_help
