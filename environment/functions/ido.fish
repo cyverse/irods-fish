4adc32433936086f1a58711a17f66e5248afcb8f
Tony Edgin <tony.edgin@gmail.com>
2020-03-20 10:36:18 -0700

    added help to ido

--- environment/functions/ido.fish
+++ environment/functions/ido.fish
@@ -1,13 +1,31 @@
-function ido --argument-names environ cmd \
+function ido \
 		--description 'executes a command in the given iRODS environment without switching to it'
 
-	if test (count $argv) -lt 2
-		printf 'Usage: ido ENVIRON CMD [ARG]...\n' >&2
-		printf 'Executes `CMD [ARG]...` in the iRODS environment ENVIRON\n\n' >&2
-		printf 'Call `ienvs` to list the available iRODS environments.\n' >&2
+	function show_help
+		echo 'Usage: ido (-h|--help|ENVIRON CMD ...)
+This command executes (CMD ...) in the iRODS environment ENVIRON without
+switching to it.
+Options are:
+ -h | --help  displays this help'
+	end
+
+	argparse --stop-nonopt --name ido (fish_opt --short h --long help) -- $argv
+	or begin
+		show_help >&2
 		return 1
 	end
 
+	if set --query _flag_h
+		show_help
+		return 0
+	end
+
+	if [ (count $argv) -lt 2 ]
+		show_help >&2
+		return 1
+	end
+
+	set environ $argv[1]
 	set cmd $argv[2..-1]
 	fish --command "iswitch $environ; and $cmd"
 end
