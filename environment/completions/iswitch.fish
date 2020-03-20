79e74bce3a03d38bf09bb4abdd213ca6634513db
Tony Edgin <tony.edgin@gmail.com>
2020-03-20 12:20:31 -0700

    iswitch completions suggest help

--- environment/completions/iswitch.fish
+++ environment/completions/iswitch.fish
@@ -1,10 +1,15 @@
 # autocompletions for iswitch
 
-function __iswitch_suggestions
-	if [ (count (commandline --cut-at-cursor --tokenize)) -eq 1 ]
-		ienvs
-	end
+
+function __iswitch_no_args
+	test (count (commandline --cut-at-cursor --tokenize)) -eq 1
 end
 
 
-complete --command iswitch --no-files --arguments '(__iswitch_suggestions)'
+complete --command iswitch --no-files
+
+complete --command iswitch --arguments '-h' --condition __iswitch_no_args --description 'shows help'
+complete --command iswitch --short-option h --long-option help --condition __iswitch_no_args \
+	--description 'shows help'
+
+complete --command iswitch --arguments '(ienvs)' --condition __iswitch_no_args
