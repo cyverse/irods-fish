4bf132fe0a242585776a4d309569f3d04c2a253f
Tony Edgin <tony.edgin@gmail.com>
2020-03-20 12:07:22 -0700

    ido completions suggest help

--- environment/completions/ido.fish
+++ environment/completions/ido.fish
@@ -1,16 +1,30 @@
 # tab completion for ido
 
-function __ido_suggestions
+function __ido_no_args
+	test (count (commandline --cut-at-cursor --tokenize)) -eq 1
+end
+
+
+function __ido_environ_suggestions
 	set cmdTerms (commandline --cut-at-cursor --tokenize)
 
 	if [ (count $cmdTerms) -eq 1 ]
 		ienvs
 	else
-		set environ $cmdTerms[2]
-		set cmd (string replace --regex -- '\s*ido\s+'"$environ"'\s+' '' (commandline))
-		ido $environ complete "--do-complete='$cmd'"
+		if [ $cmdTerms[2] != -h ]
+		and [ $cmdTerms[2] != --help ]
+			set environ $cmdTerms[2]
+			set cmd (string replace --regex -- '\s*ido\s+'"$environ"'\s+' '' (commandline))
+			ido $environ complete "--do-complete='$cmd'"
+		end
 	end
 end
 
 
-complete --command ido --no-files --arguments '(__ido_suggestions)'
+complete --command ido --no-files
+
+complete --command ido --arguments '-h' --condition __ido_no_args --description 'shows help'
+complete --command ido --short-option h --long-option help --condition __ido_no_args \
+	--description 'shows help'
+
+complete --command ido --arguments '(__ido_environ_suggestions)'
