df7bd127762e6939f96ceca1164e0c59e45c1dee
Tony Edgin <tedgin@cyverse.org>
2023-03-17 13:35:40 -0700

    reformattted file

--- environment/completions/functions/__irods_env_help_completion.fish
+++ environment/completions/functions/__irods_env_help_completion.fish
@@ -1,11 +1,11 @@
 function __irods_env_help_completion --argument-names cmd \
-		--description 'creates the help completions for a given command'
+    --description 'creates the help completions for a given command'
 
-	complete --command $cmd --short-option h --long-option help \
-    --condition "__irods_no_args_condition (__irods_tokenize_cmdline h '')" \
-		--description 'shows help'
+    complete --command $cmd --short-option h --long-option help \
+        --condition "__irods_no_args_condition (__irods_tokenize_cmdline h '')" \
+        --description 'shows help'
 
-  complete --command $cmd --arguments '--help' \
-  	--condition "__irods_no_args_condition (__irods_tokenize_cmdline h '')" \
-	  --description 'shows help'
+    complete --command $cmd --arguments --help \
+        --condition "__irods_no_args_condition (__irods_tokenize_cmdline h '')" \
+        --description 'shows help'
 end
